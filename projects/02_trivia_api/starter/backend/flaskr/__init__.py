38cdcb5c5c8193efb2a3ee1281e382da63732dba
Justin Ng <jng@slack-corp.com>
2020-02-11 10:37:00 -0700

    Introduce error handling for 400

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -232,6 +232,14 @@ def create_app(test_config=None):
   Create error handlers for all expected errors 
   including 404 and 422. 
   '''
+  @app.errorhandler(400)
+  def bad_request(error):
+    return jsonify({
+          "success": False,
+          "error": 400,
+          "message": "Bad request"
+        }), 400
+
   @app.errorhandler(404)
   def not_found(error):
     return jsonify({
@@ -246,7 +254,7 @@ def create_app(test_config=None):
           "success": False,
           "error": 422,
           "message": "Method not allowed"
-        }), 404
+        }), 422
 
   @app.errorhandler(500)
   def internal_server_error(error):
